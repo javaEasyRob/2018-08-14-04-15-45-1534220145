d708b124c05e770d078bdc478afe62394d64db4a
xjy <Xin18792900229@163.com>
2019-05-28 15:52:10 +0800

    update

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,9 +1,5 @@
 const main = require('../main/main');
 
-describe('main()', () => {
-
-    it('should calculate the remaindar', () => {
-        expect(main(9, 3)).toBe(0);
-        expect(main(11, 4)).toBe(3);
-    });
-});
+function main(num1, num2) {
+    console.log(num1%num2);
+}
